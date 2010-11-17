e9af32aabf5a337a9a6a95281a7afed2fd18e634
Shawn Tan <shawn.tan@aeste.my>
2010-11-17 21:55:32 +0800

    Fixed header and footer links.

--- aeste.wp/footer.php
+++ aeste.wp/footer.php
@@ -2,22 +2,18 @@
 <div id="footer" class="row">
    <div id="lfoot" class="column grid_3">
    <div id="addr" class="">
-   <b>Aeste Works (M) Sdn Bhd</b><br/>
-   6-3-6 Queen's Avenue<br/>
+   <b>Aeste Works (M) S/B</b><br/>
+   6-3-6 Queens Avenue<br/>
    Jalan Bayam, Cheras<br/>
    55100 Kuala Lumpur, Malaysia<br/>
    info&#64;aeste.my<br/>
    </div>
    </div><!--lfoot-->
-
 <div id="rfoot" class="column grid_9">
     <ul class="nav">
-    <li><a href="http://www.aeste.my">Home</a></li>
-    <li><a href="http://blog.aeste.my">Blog</a></li>
-    <li><a href="http://wiki.aeste.my">Wiki</a></li>
-    <li><a href="http://www.aeste.my">Home</a></li>
-    <li><a href="http://blog.aeste.my">Blog</a></li>
-    <li><a href="http://wiki.aeste.my">Wiki</a></li>
+    <li><a href="http://www.aeste.my/careers">Careers</a></li>
+    <li><a href="http://www.aeste.my/donate">Contribute</a></li>
+    <li><a href="http://www.aeste.my/contactus">Contact Us</a></li>
     </ul>
 
   <div id="copyright">
